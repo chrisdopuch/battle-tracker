f0f928f99ae1485239c11808e92d7eb17eac5c48
Chris Dopuch <cdopuch@appnexus.com>
2016-10-09 19:42:22 -0700

    adds react to the view

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,7 +11,7 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
@@ -19,3 +19,17 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+class BattleTracker extends React.Component {
+  render() {
+    return (<h1>Battle Tracker!</h1>);
+  }
+}
+
+ReactDOM.render(
+<BattleTracker/>,
+  document.getElementById("container")
+);
